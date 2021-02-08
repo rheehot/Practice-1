047aa0eab909ab76379c1f82eb910423bc525c91
zulie <reo.momm@gmail.com>
2021-02-08 18:13:15 +0900

    wip

--- src/pages/compare/CompareButton.js
+++ src/pages/compare/CompareButton.js
@@ -1,50 +1,50 @@
-import React, { Fragment, useState, useEffect } from "react";
-import styled from "styled-components";
-import ModalButton from "./ModalButton";
-import { API } from "../config";
+import React, { Fragment, useState, useEffect } from 'react';
+import styled from 'styled-components';
+import ModalButton from './ModalButton';
+import { API } from '../config';
 
 const Button = styled.button``;
 
 const CompareButton = () => {
-  const [toggle, setToggle] = useState(false);
-  const [isModal, setIsModal] = useState(false);
-  const [isModalProduct, setIsModalProduct] = useState(false);
-  const [data, setData] = useState();
+    const [toggle, setToggle] = useState(false);
+    const [isModal, setIsModal] = useState(false);
+    const [isModalProduct, setIsModalProduct] = useState(false);
+    const [data, setData] = useState();
 
-  useEffect(() => {
-    fetch(API)
-      .then((res) => res.json())
-      .then((res) => setData(res.data.content));
-    //   .then((res) => console.log(res));
-  }, []);
+    useEffect(() => {
+        fetch(API)
+            .then((res) => res.json())
+            .then((res) => setData(res.data.content));
+        //   .then((res) => console.log(res));
+    }, []);
 
-  console.log(data);
+    console.log(data);
 
-  const handleShowModal = () => {
-    setIsModal(!isModal);
-  };
+    const handleShowModal = () => {
+        setIsModal(!isModal);
+    };
 
-  const handleModalProduct = () => {
-    setIsModalProduct(!isModalProduct);
-  };
+    const handleModalProduct = () => {
+        setIsModalProduct(!isModalProduct);
+    };
 
-  const handleToggle = () => {
-    setToggle(!toggle);
-  };
+    const handleToggle = () => {
+        setToggle(!toggle);
+    };
 
-  return (
-    <Fragment>
-      <Button
-        onClick={handleToggle}
-        isModal={isModal}
-        handleshowModal={handleShowModal}
-        handleModalProduct={handleModalProduct}
-      >
-        비교
-      </Button>
-      <ModalButton data={data} />
-    </Fragment>
-  );
+    return (
+        <Fragment>
+            <Button
+                onClick={handleToggle}
+                isModal={isModal}
+                handleshowModal={handleShowModal}
+                handleModalProduct={handleModalProduct}
+            >
+                비교
+            </Button>
+            <ModalButton data={data} />
+        </Fragment>
+    );
 };
 
 export default CompareButton;
